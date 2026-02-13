2286129cb0129cb8b8873a1adad0ec2964df93a7
Guillaume Bzh <guillaume4cebil@gmail.com>
2026-02-13 13:39:00 +0100

    feat(script.js) add carousel img and dots on banner-img oprational

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,45 @@
+
 const arrowLeft = document.querySelector(".arrow_left")
 const arrowRight = document.querySelector(".arrow_right")
 
+
 const dotSelected = document.querySelector(".dot_selected")
 const dots = document.querySelectorAll(".dot")
-let currentIndex = 0
-let i = currentIndex
+let i = 0
+
+const banner = document.querySelector(".banner-img")
+const tagLine = document.querySelector("#banner p")
 
 
-const back = arrowLeft.addEventListener("click", () => {
+
+
+/********************************************** Tableaux Slides Des Images + Tags *******************************/
+
+
+const slides = [
+	{
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+
+
+/************************************** Carrousel Image Banner ***************************/
+
+
+arrowLeft.addEventListener("click", () => {
 
 	dots[i].classList.remove("dot_selected")
 
@@ -15,11 +47,13 @@ const back = arrowLeft.addEventListener("click", () => {
 
 	dots[i].classList.add("dot_selected")
 
+	banner.setAttribute("src", "../assets/images/slideshow/" + slides[i].image)
+	tagLine.innerHTML = slides[i].tagLine
 })
 
 
 
-arrowRight.addEventListener("click", (event) => {
+arrowRight.addEventListener("click", () => {
 
 
 	dots[i].classList.remove("dot_selected")
@@ -27,56 +61,11 @@ arrowRight.addEventListener("click", (event) => {
 	i = (i + 1) % dots.length
 
 	dots[i].classList.add("dot_selected")
+
+	banner.setAttribute("src", "../assets/images/slideshow/" + slides[i].image)
+	tagLine.innerHTML = slides[i].tagLine
 })
 
-const banner = document.querySelector(".banner-img")
-const slides2 = document.querySelector(".slides2")
-const slides3 = document.querySelector(".slides3")
-const slides4 = document.querySelector(".slides4")
-
-
-// const slides2Img = ` <img src = "./images/slideshow/slide2.jpg" alt = "tirage HD grand format"> `
-// slides2.innerHTML = slides2Img
 
 
 
-function dotSlide() {
-	if (next) {
-
-	}
-}
-
-
-function slides() {
-	if (next) {
-		for (i = 1; i < 4; i++) {
-			let slideIMG = ` <img src = "./images/slideshow/slide2.jpg" alt = "tirage HD grand format"> `
-			banner = slideIMG
-			slideImg
-			i++
-		}
-	}
-}
-
-
-
-
-
-// const slides = [
-// 	{
-// 		"image": "slide1.jpg",
-// 		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
-// 	},
-// 	{
-// 		"image": "slide2.jpg",
-// 		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-// 	},
-// 	{
-// 		"image": "slide3.jpg",
-// 		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
-// 	},
-// 	{
-// 		"image": "slide4.png",
-// 		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
-// 	}
-// ]
