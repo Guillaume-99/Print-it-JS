1c5997d127566f1a76d532052aa1012b7eb50264
Guillaume Bzh <guillaume4cebil@gmail.com>
2026-02-13 15:32:01 +0100

    refractor(index and script) add main.js on index and split script.js into main

--- assets/script.js
+++ assets/script.js
@@ -36,43 +36,7 @@ const slides = [
 ]
 
 
-/************************************** Carrousel Image Banner ***************************/
 
-
-arrowLeft.addEventListener("click", () => {
-
-	dots[i].classList.remove("dot_selected")
-
-	if (i === 0) {
-		i = dots.length - 1
-	} else {
-		i = i - 1
-	}
-
-	dots[i].classList.add("dot_selected")
-
-	banner.setAttribute("src", "../assets/images/slideshow/" + slides[i].image)
-	tagLine.innerHTML = slides[i].tagLine
-
-})
-
-
-
-arrowRight.addEventListener("click", () => {
-
-	dots[i].classList.remove("dot_selected")
-
-	if (i === 3) {
-		i = 0
-	} else {
-		i = i + 1
-	}
-
-	dots[i].classList.add("dot_selected")
-
-	banner.setAttribute("src", "../assets/images/slideshow/" + slides[i].image)
-	tagLine.innerHTML = slides[i].tagLine
-})
 
 
 
