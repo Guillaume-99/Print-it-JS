518009970246ef5148a21bd4cc7f79c34143b093
Guillaume Bzh <guillaume4cebil@gmail.com>
2026-02-18 11:25:12 +0100

    refractor(script/main) remove main.js to script.js add function init

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@ const arrowRight = document.querySelector(".arrow_right")
 
 
 const dots = document.querySelector(".dots")
+
 let i = 0
 
 const banner = document.querySelector(".banner-img")
@@ -32,3 +33,87 @@ const slides = [
 		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+function createDot() {
+	// Creation des dots
+	//parcourir les images dans le tableau sliders
+
+	//pour chaque élément du tableau, créer un dot
+	//inserer les dots dans le DOM
+
+	for (let i = 0; i < slides.length; i++) {
+
+		const dot = document.createElement("div")
+		dot.classList.add("dot")
+
+		if (i === 0) {
+			dot.classList.add("dot_selected")
+		}
+
+		dots.appendChild(dot)
+	}
+}
+
+/************************************** Carrousel Image Banner ***************************/
+
+
+
+function back() {
+
+	arrowLeft.addEventListener("click", () => {
+
+		dotElements[i].classList.remove("dot_selected")
+
+		if (i === 0) {
+			i = slides.length - 1
+		} else {
+			i = i - 1
+		}
+
+		dotElements[i].classList.add("dot_selected")
+
+		banner.setAttribute("src", "./assets/images/slideshow/" + slides[i].image)
+
+		tagLine.innerHTML = slides[i].tagLine
+	}
+
+	)
+}
+
+function next() {
+
+	arrowRight.addEventListener("click", () => {
+
+		dotElements[i].classList.remove("dot_selected")
+
+		if (i === slides.length - 1) {
+			i = 0
+		} else {
+			i = i + 1
+		}
+
+		dotElements[i].classList.add("dot_selected")
+
+		banner.setAttribute("src", "./assets/images/slideshow/" + slides[i].image)
+
+		tagLine.innerHTML = slides[i].tagLine
+	}
+	)
+}
+
+function navCarousel() {
+
+	back()
+	next()
+}
+
+
+// Logique a executer apres chargement de la page
+
+function init() {
+	createDot()
+	navCarousel()
+}
+init()
+const dotElements = document.querySelectorAll(".dot")
